2865ded1c1cb4a65171db77a72845126861ae7ad
ASHMITHA PAIS <ashdale30@gmail.com>
2024-05-12 21:23:21 -0700

    Fixing front end missing code

--- ApplicationSJ/Back-End/SJApp/myapi/models.py
+++ ApplicationSJ/Back-End/SJApp/myapi/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from pgvector.django import VectorField
+from pgvector.django import HnswIndex
+
+
+class File(models.Model):
+    # ... other fields ...
+    embedding_clip_vit_l_14 = VectorField(
+        dimensions=768,
+        help_text="Vector embeddings (clip-vit-large-patch14) of the file content",
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        indexes = [
+            HnswIndex(
+                name="clip_l14_vectors_index",
+                fields=["embedding_clip_vit_l_14"],
+                m=16,
+                ef_construction=64,
+                opclasses=["vector_cosine_ops"],
+            )
+        ]
