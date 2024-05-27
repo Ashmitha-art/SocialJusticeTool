c71e55cea6fb7169a30513a1323466f3bfe4baea
Isiah Paul-McGlothin <ipaulmcglothin@mail.sfsu.edu>
2024-05-27 09:49:09 -0700

    frontend revamp

--- ApplicationSJ/front-end/src/App.js
+++ ApplicationSJ/front-end/src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
-import FileUpload from './Components/Home/FileUpload'
-import SearchBar from './Components/Home/SearchBar'
+import FileUpload from './Components/Home/FileUpload';
+import SearchBar from './Components/Home/SearchBar';
 import AnswerDisplay from './Components/Home/AnswerDisplay';
+import ResponsiveAppBar from './Components/appbar'; 
 
 function App() {
   const [file, setFile] = useState(null);
@@ -13,29 +14,29 @@ function App() {
   };
 
   const handleUpload = () => {
-  if (file) {
-    // Create a FormData object and append the file to it
-    const formData = new FormData();
-    formData.append('file', file);
+    if (file) {
+      // Create a FormData object and append the file to it
+      const formData = new FormData();
+      formData.append('file', file);
 
-    // Call the backend Django view to upload the file
-    fetch('http://127.0.0.1:8000/api/upload/', {
-      method: 'POST',
-      body: formData,
-      // No need to set Content-Type header, FormData will set it automatically
-    })
-    .then(response => {
-      // Handle response from backend
-      console.log('File uploaded successfully');
-    })
-    .catch(error => {
-      // Handle error
-      console.error('Error uploading file:', error);
-    });
-  } else {
-    console.error('No file selected');
-  }
-};
+      // Call the backend Django view to upload the file
+      fetch('http://127.0.0.1:8000/api/upload/', {
+        method: 'POST',
+        body: formData,
+        // No need to set Content-Type header, FormData will set it automatically
+      })
+      .then(response => {
+        // Handle response from backend
+        console.log('File uploaded successfully');
+      })
+      .catch(error => {
+        // Handle error
+        console.error('Error uploading file:', error);
+      });
+    } else {
+      console.error('No file selected');
+    }
+  };
 
   const handleSearch = (searchTerm) => {
     const randomAnswer = Math.random() > 0.5 ? 'Yes' : 'No';
@@ -43,11 +44,14 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <h1 className="title">Social Justice Tool</h1>
-      <FileUpload onFileChange={handleFileChange} onUpload={handleUpload} />
-      <SearchBar onSearch={handleSearch} />
-      <AnswerDisplay answers={answers} />
+    <div>
+      <ResponsiveAppBar /> {/* AppBar added to the top of the page */}
+      <div className="container">
+        <h1 className="title">Social Justice Tool</h1>
+        <FileUpload onFileChange={handleFileChange} onUpload={handleUpload} />
+        <SearchBar onSearch={handleSearch} />
+        <AnswerDisplay answers={answers} />
+      </div>
     </div>
   );
 }
