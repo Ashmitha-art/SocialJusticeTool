cd6caa94965dc656cf2e00e5bc192435d718d8ad
ASHMITHA PAIS <ashdale30@gmail.com>
2024-11-09 11:17:18 -0800

    Dummy static data added to section 1 and section 2

--- ApplicationSJ/front-end/src/Components/Home/SectionOne.js
+++ ApplicationSJ/front-end/src/Components/Home/SectionOne.js
@@ -5,6 +5,11 @@ function SectionOne() {
     <div>
       <h2>Section One</h2>
       <p>Content for Section One.</p>
+      <img
+        src={`${process.env.PUBLIC_URL}/weather_heatmap.png`} // Correct path from the public folder
+        alt="Weather Heatmap"
+        style={{ width: "100%", height: "30%" }}
+      />
     </div>
   );
 }
--- ApplicationSJ/front-end/src/Components/Home/SectionTwo.js
+++ ApplicationSJ/front-end/src/Components/Home/SectionTwo.js
@@ -5,6 +5,11 @@ function SectionTwo() {
     <div>
       <h2>Section Two</h2>
       <p>Content for Section Two.</p>
+      <img
+        src={`${process.env.PUBLIC_URL}/weather_heatmap.png`} // Correct path from the public folder
+        alt="Weather Heatmap"
+        style={{ width: "100%", height: "30%" }}
+      />
     </div>
   );
 }
